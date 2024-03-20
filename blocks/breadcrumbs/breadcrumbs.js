8a524da4571fe800b52eaebd966c52b7ff45a42d
Vivek Singh <vivesing@adobe.com>
2024-03-20 16:03:34 +0530

    Breadcrumbs use data from word file directly

--- blocks/breadcrumbs/breadcrumbs.js
+++ blocks/breadcrumbs/breadcrumbs.js
@@ -1,59 +1,22 @@
-const fetchBreadCrumbs = async () => {
-  const breadcrumbsDataURL = '/draft/vivesing/breadcrumbs.json';
-  let hostUrl = window.location.origin;
-  if (!hostUrl || hostUrl === 'null') {
-    // eslint-disable-next-line prefer-destructuring
-    hostUrl = window.location.ancestorOrigins[0];
-  }
-  const apiUrl = `${hostUrl}${breadcrumbsDataURL}`;
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    // Transform the API response to the desired breadcrumbs map format
-    const breadcrumbsList = data && data.data;
-    const breadcrumbsMap = {};
-    Array.from(breadcrumbsList).forEach((singleBreadCrumb) => {
-      breadcrumbsMap[singleBreadCrumb.path] = singleBreadCrumb;
-    });
-    return breadcrumbsMap;
-  } catch (error) {
-    return [];
-  }
-};
-
-const decorateBreadCrumbs = async (block) => {
-  const breadcrumbContainerDiv = document.createElement('div');
-  breadcrumbContainerDiv.className = 'breadcrumbs-list';
-  const breadcrumbsMap = await fetchBreadCrumbs();
-  const currentPath = window.location.pathname;
-  let currPath = '';
-  currentPath.split('/').forEach((singleLevel, index, arr) => {
-    if (singleLevel === '') {
-      currPath = `${currPath}/`;
-    } else {
-      currPath = `${currPath}${singleLevel}`;
-    }
-    const singleItemLinkTag = document.createElement('a');
-    singleItemLinkTag.className = 'breadcrumb-link';
-    const details = breadcrumbsMap[currPath] || {};
-    // fallback to the path value in case missing in excel
-    const itemTitle = details.title || singleLevel;
-    const itemLink = details.url || window.location.href;
-    singleItemLinkTag.href = itemLink;
-    const singleItemTitleDiv = document.createElement('div');
-    singleItemTitleDiv.className = 'breadcrumb-title';
-    singleItemTitleDiv.innerText = itemTitle;
+/**
+ * decorates the breadcrumbs
+ * @param {Element} block The breadcrumbs block element
+ */
+const decorateBreadCrumbs = (block) => {
+  [...block.children].forEach((singleItem, index, arr) => {
+    const linkTag = singleItem.querySelector('a');
+    linkTag.classList.add('breadcrumb-link');
+    // Make the last item of the breadcrumb as the active one
     if (index === arr.length - 1) {
-      singleItemLinkTag.classList.add('active');
+      linkTag.classList.add('active');
     }
-    singleItemLinkTag.appendChild(singleItemTitleDiv);
-    breadcrumbContainerDiv.appendChild(singleItemLinkTag);
-    currPath = index === 0 ? currPath : `${currPath}/`;
+    const breadcrumbTitle = linkTag.innerText;
+    linkTag.innerText = '';
+    const breadcrumbItemDiv = document.createElement('div');
+    breadcrumbItemDiv.className = 'breadcrumb-title';
+    breadcrumbItemDiv.innerText = breadcrumbTitle;
+    linkTag.appendChild(breadcrumbItemDiv);
   });
-  block.append(breadcrumbContainerDiv);
 };
 
 /**
