ab4f743f3a1f56d61c0d7c88995a15dec734fd90
anagarwa <anagarwa@adobe.com>
2024-03-27 11:32:27 +0530

    adding bing

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -34,7 +34,7 @@ function includeExternalScripts() {
   img.setAttribute('height', '0');
   img.setAttribute('alt', '');
   img.setAttribute('src', 'https://bat.bing.com/action/0?ti=20131149&amp;tm=gtm002&amp;Ver=2&amp;mid=354fa5f9-ca7f-4f18-9cdd-806a73b03b1e&amp;sid=89f10060eb3411ee88915ff977266d91&amp;vid=cb7c5cb0293a11ee81982d50267aae77&amp;vids=0&amp;msclkid=N&amp;pi=918639831&amp;lg=en-GB&amp;sw=1920&amp;sh=1080&amp;sc=24&amp;tl=Equity%20Research%20%26%20Analysis%20for%20Smooth%20Equity%20Trading%20%7C%20ICICI%20Direct&amp;p=https%3A%2F%2Fwww.icicidirect.com%2Fresearch%2Fequity&amp;r=&amp;lt=7571&amp;evt=pageLoad&amp;sv=1&amp;rn=767940');
-  img.setAttribute('style', 'width: 0px; height: 0px; display: none; visibility: hidden;');
+  img.setAttribute('style', 'width: 0px; height: 0px');
 
   // Append the img element to the div
   bingdiv.appendChild(img);
