dc989d972071854546b25e224adb705d97f403ea
Sneh Arora <sneh@adobe.com>
2024-04-08 15:35:37 +0530

    Added captcha

---
+++ scripts/recaptcha.js
@@ -0,0 +1,53 @@
+export default class GoogleReCaptcha {
+  id;
+
+  siteKey;
+
+  loadPromise;
+
+  constructor(siteKey, id) {
+    this.siteKey = siteKey;
+    this.id = id;
+  }
+
+  #loadScript(url) {
+    if (!this.loadPromise) {
+      this.loadPromise = new Promise((resolve, reject) => {
+        const head = document.head || document.querySelector('head');
+        const script = document.createElement('script');
+        script.src = url;
+        script.async = true;
+        script.onload = () => resolve(window.grecaptcha);
+        script.onerror = () => reject(new Error(`Failed to load script ${url}`));
+        head.append(script);
+      });
+    }
+  }
+
+  loadCaptcha(selector) {
+    if (this.siteKey) {
+      const obs = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.#loadScript(`https://www.google.com/recaptcha/api.js?render=${this.siteKey}`);
+            obs.disconnect();
+          }
+        });
+      });
+      obs.observe(selector);
+    }
+  }
+
+  async getToken() {
+    if (!this.siteKey) {
+      return null;
+    }
+    return new Promise((resolve) => {
+      const { grecaptcha } = window;
+      grecaptcha.ready(async () => {
+        const token = await grecaptcha.execute(this.siteKey, { action: 'submit' });
+        resolve(token);
+      });
+    });
+  }
+}
