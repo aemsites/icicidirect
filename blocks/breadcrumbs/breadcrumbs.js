c9511c87dfda23f1cbc244062ad0dd66bfc8c409
anagarwa <anagarwa@adobe.com>
2024-04-19 00:04:10 +0530

    fix: adding seo schema (#172)

--- blocks/breadcrumbs/breadcrumbs.js
+++ blocks/breadcrumbs/breadcrumbs.js
@@ -1,8 +1,30 @@
+async function appendSchemaDataToBody(schemaData) {
+  // Create a JSON LD object based on schemaData
+  const jsonLD = {
+    '@context': 'https://schema.org/',
+    '@type': 'BreadcrumbList',
+    itemListElement: schemaData.map((item, index) => ({
+      '@type': 'ListItem',
+      position: index + 1,
+      name: item.name,
+      item: item.item ? item.item : 'default_value_here',
+    })),
+  };
+
+  // Create a script element
+  const script = document.createElement('script');
+  script.type = 'application/ld+json';
+  script.textContent = JSON.stringify(jsonLD);
+
+  // Append the script element to the body tag
+  document.body.appendChild(script);
+}
 /**
  * decorates the breadcrumbs, mainly the body's top breadcrumbs
  * @param {Element} block The breadcrumbs block element
  */
 export default async function decorate(block) {
+  const schemaData = [];
   [...block.children].forEach((singleItem, index, arr) => {
     const linkTag = singleItem.querySelector('a');
     linkTag.classList.add('breadcrumb-link');
@@ -16,5 +38,10 @@ export default async function decorate(block) {
     breadcrumbItemDiv.className = 'breadcrumb-title';
     breadcrumbItemDiv.innerText = breadcrumbTitle;
     linkTag.appendChild(breadcrumbItemDiv);
+    schemaData.push({
+      name: breadcrumbTitle,
+      item: linkTag.href ? linkTag.href : window.location.origin,
+    });
   });
+  appendSchemaDataToBody(schemaData);
 }
--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -1,6 +1,42 @@
 import { createElement } from '../../scripts/blocks-utils.js';
 import { readBlockConfig } from '../../scripts/aem.js';
 
+async function generateFAQSchema(faqData) {
+  // Create an array to store Question-Answer pairs
+  const faqArray = [];
+
+  // Loop through each item in faqData
+  faqData.forEach((item) => {
+    // Create a Question-Answer object
+    const qaObject = {
+      '@type': 'Question',
+      name: item.name,
+      acceptedAnswer: {
+        '@type': 'Answer',
+        text: item.text,
+      },
+    };
+
+    // Push the Question-Answer object to the faqArray
+    faqArray.push(qaObject);
+  });
+
+  // Create the JSON-LD object
+  const jsonLD = {
+    '@context': 'https://schema.org',
+    '@type': 'FAQPage',
+    mainEntity: faqArray,
+  };
+
+  // Create a script element
+  const script = document.createElement('script');
+  script.type = 'application/ld+json';
+  script.textContent = JSON.stringify(jsonLD);
+
+  // Append the script element to the body tag
+  document.body.appendChild(script);
+}
+
 function addEvent(faqTitle, block) {
   faqTitle.addEventListener('click', () => {
     if (!faqTitle.classList.contains('visible')) {
@@ -27,13 +63,18 @@ function decorateTitle(faqTitle, title) {
   faqTitle.append(titleTag);
 }
 
-function decorateContent(faqContents, block) {
+function decorateContent(faqContents, block, schemaData) {
   [...faqContents.children].forEach((faqContent, index) => {
-    if (index > 4) {
-      faqContent.classList.add('no-visible');
-    }
     const itemTitle = faqContent.firstElementChild;
     const itemContent = faqContent.lastElementChild;
+    if (index > 4) {
+      faqContent.classList.add('no-visible');
+    } else {
+      schemaData.push({
+        name: itemTitle.textContent,
+        text: itemContent.textContent,
+      });
+    }
     if (itemTitle) {
       itemTitle.classList.add('faq-item-title');
       const i = createElement('i', '');
@@ -92,10 +133,12 @@ export default async function decorate(block) {
     }
   });
   decorateTitle(faqTitle, topTitle);
-  decorateContent(faqContent, block);
+  const schemaData = [];
+  decorateContent(faqContent, block, schemaData);
   decorateButton(faqButton, block, buttonTitle, expendButtonTitle);
 
   block.replaceChildren(faqTitle);
   block.appendChild(faqContent);
   block.appendChild(faqButton);
+  generateFAQSchema(schemaData);
 }
