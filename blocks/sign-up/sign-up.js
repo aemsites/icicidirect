af17769efdec70360e9eb951452d8da270d300f1
Vivek Singh <vivesing@adobe.com>
2024-05-02 16:15:02 +0530

    Add sign up block hero variant

---
+++ blocks/sign-up/sign-up.js
@@ -0,0 +1,157 @@
+import { readBlockConfig } from '../../scripts/aem.js';
+import { createElement } from '../../scripts/blocks-utils.js';
+
+/**
+ * Handler for submit button click
+ * @param {*} event
+ */
+const handleOpenAccountSubmit = async (event, block) => {
+  event.preventDefault();
+  const mobileNumberInput = block.querySelector('.sign-up .phonenumber-textbox');
+  const mobileNumber = mobileNumberInput.value;
+  const accountCreationUrl = event.target.href;
+  // Check for valid mobile number format
+  const mobileRegex = /^([0]|\+91)?[6789]\d{9}$/;
+  const validationMessage = block.querySelector('.signup-container .error-message');
+  if (!mobileNumber || mobileNumber.length < 10 || !mobileRegex.test(mobileNumber)) {
+    validationMessage.classList.add('invalid');
+  } else {
+    validationMessage.classList.remove('invalid');
+    // const token = window.validateCaptchaToken;
+    window.location.href = `${accountCreationUrl}?mobile=${mobileNumber}`;
+  }
+};
+
+/**
+ * Removes non-numeric characters from the input value.
+ * @param {Event} event - The event object triggered by the input element.
+ */
+function blockNonNumbers(event) {
+  const inputElement = event.target;
+  inputElement.value = inputElement.value.replace(/[^0-9]/g, '');
+}
+
+function createMobileNumberInput(placeholderText) {
+  const inputElement = createElement('input', 'phonenumber-textbox');
+  inputElement.type = 'text';
+  inputElement.placeholder = placeholderText;
+  inputElement.maxLength = '10';
+  inputElement.pattern = '[0-9]*';
+  inputElement.autocomplete = 'off';
+  inputElement.addEventListener('input', blockNonNumbers);
+  return inputElement;
+}
+
+function createSubmitButton(block, buttontitle, accountcreationurl) {
+  const submitButton = createElement('button', 'signupbtn');
+  submitButton.textContent = buttontitle;
+  submitButton.href = accountcreationurl;
+  submitButton.addEventListener('click', (event) => { handleOpenAccountSubmit(event, block); });
+  return submitButton;
+}
+
+function createErrorSpan(errormessage) {
+  const spanElement = createElement('span', 'error-message');
+  spanElement.textContent = errormessage;
+  return spanElement;
+}
+
+function createTitle(titleHTML) {
+  if (!titleHTML) {
+    return null;
+  }
+  const col1Div = createElement('div', 'signupsections');
+  const titleWrapDiv = createElement('div', 'title-wrap');
+  const h2Element = createElement('h2', '');
+  h2Element.innerHTML = titleHTML.innerHTML;
+  h2Element.classList.add('text-left');
+  titleWrapDiv.appendChild(h2Element);
+  col1Div.appendChild(titleWrapDiv);
+  return col1Div;
+}
+
+function createReCaptcha() {
+  const captchadiv = createElement('div', 'g-recaptcha');
+  captchadiv.id = 'captchadiv';
+  return captchadiv;
+}
+
+function createSignUpElement(
+  block,
+  signupString,
+  promotionaltext,
+  placeholderText,
+  buttontitle,
+  errormessage,
+  accountcreationurl,
+) {
+  const signupFormDiv = createElement('div', 'signupsections');
+
+  const formGroupDiv = createElement('div', 'signup-form-group');
+  formGroupDiv.classList.add('text-center');
+
+  const signupStringElement = createElement('label', '');
+  signupStringElement.innerHTML = signupString.innerHTML;
+
+  const promotionalSpan = createElement('span', 'promotional-text');
+  promotionalSpan.textContent = promotionaltext;
+
+  const formFieldsDiv = createElement('div', '');
+  const mobileInput = createMobileNumberInput(placeholderText);
+
+  const submitButton = createSubmitButton(block, buttontitle, accountcreationurl);
+  const errorSpan = createErrorSpan(errormessage);
+  const captcha = createReCaptcha();
+  formFieldsDiv.appendChild(mobileInput);
+  formFieldsDiv.appendChild(captcha);
+  formFieldsDiv.appendChild(submitButton);
+
+  formGroupDiv.appendChild(signupStringElement);
+  formGroupDiv.appendChild(promotionalSpan);
+  formGroupDiv.appendChild(formFieldsDiv);
+  formGroupDiv.appendChild(errorSpan);
+
+  signupFormDiv.appendChild(formGroupDiv);
+  return signupFormDiv;
+}
+
+export default async function decorate(block) {
+  const blockConfig = readBlockConfig(block);
+  const {
+    title, promotionaltext, placeholdertext, buttontitle, errormessage, accountcreationurl,
+  } = blockConfig;
+  let titleHTML;
+  let signupstringHTML;
+  if (title) {
+    [, titleHTML] = block.querySelectorAll(':scope > div')[0].children;
+    [, signupstringHTML] = block.querySelectorAll(':scope > div')[1].children;
+  } else {
+    [, signupstringHTML] = block.querySelectorAll(':scope > div')[0].children;
+  }
+  const sectionDiv = createElement('div', 'section');
+  sectionDiv.classList.add('margin', 'signup-container');
+
+  const articleElement = createElement('article', '');
+  const rowDiv = createElement('div', 'row');
+  if (!block.classList.contains('hero')) {
+    rowDiv.classList.add('justify-content-center', 'align-items-center');
+  }
+  const titleField = createTitle(titleHTML);
+  const signupElementDiv = createSignUpElement(
+    block,
+    signupstringHTML,
+    promotionaltext,
+    placeholdertext,
+    buttontitle,
+    errormessage,
+    accountcreationurl,
+  );
+  if (titleField) {
+    rowDiv.appendChild(titleField);
+  }
+  rowDiv.appendChild(signupElementDiv);
+  articleElement.appendChild(rowDiv);
+  sectionDiv.appendChild(articleElement);
+  block.textContent = '';
+  block.appendChild(sectionDiv);
+}
