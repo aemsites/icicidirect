b68fbde15a5825d4e92a9f1c3f58900f86d33a32
anagarwa <anagarwa@adobe.com>
2024-05-08 19:42:55 +0530

    fix: adding old at scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -173,18 +173,18 @@ async function getElementForOffer(offer) {
 }
 
 async function getAndApplyOffers() {
-  // const response = await window.adobe.target.getOffers({ request: { execute: { pageLoad: {} } } });
-  // const { options = [], metrics = [] } = response.execute.pageLoad;
-  // onDecoratedElement(() => {
-  //   window.adobe.target.applyOffers({ response });
-  //   // keeping track of offers that were already applied
-  //   options.forEach((o) => o.content = o.content.filter((c) => !getElementForOffer(c)));
-  //   // keeping track of metrics that were already applied
-  //   metrics.map((m, i) => getElementForMetric(m) ? i : -1)
-  //       .filter((i) => i >= 0)
-  //       .reverse()
-  //       .map((i) => metrics.splice(i, 1));
-  // });
+  const response = await window.adobe.target.getOffers({ request: { execute: { pageLoad: {} } } });
+  const { options = [], metrics = [] } = response.execute.pageLoad;
+  onDecoratedElement(() => {
+    window.adobe.target.applyOffers({ response });
+    // keeping track of offers that were already applied
+    options.forEach((o) => o.content = o.content.filter((c) => !getElementForOffer(c)));
+    // keeping track of metrics that were already applied
+    metrics.map((m, i) => getElementForMetric(m) ? i : -1)
+        .filter((i) => i >= 0)
+        .reverse()
+        .map((i) => metrics.splice(i, 1));
+  });
 }
 
 let atjsPromise = Promise.resolve();
