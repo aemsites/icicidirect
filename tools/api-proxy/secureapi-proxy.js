381cbd9984eda63c319152ba0030598c6a497b37
anagarwa <anagarwa@adobe.com>
2024-05-09 17:08:09 +0530

    fix : updated worker code

--- tools/api-proxy/secureapi-proxy.js
+++ tools/api-proxy/secureapi-proxy.js
@@ -8,36 +8,102 @@
  * @param env
  * @returns {Promise<Response>}
  */
-
+/* eslint-disable */
 // eslint-disable-next-line no-unused-vars
+    // e
+const TOKEN_ID = 'http://icicidirect.finoux.com/CDNResearchAPI/CallResearchAPI.TokenId';
+
+const tokenRequest = new Request(TOKEN_ID, {
+  method: 'GET'
+});
+
+async function getSessionData(url, cache, env) {
+  // Define the request body
+  const requestBody = {
+    apiName: "GenerateSession"
+  };
+
+  // Make a POST request to the API to fetch session data
+  const apiurl = 'http://icicidirect.finoux.com/CDNResearchAPI/CallResearchAPI';
+  const response = await fetch(apiurl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': env.Authorization_Research,
+    },
+    body: JSON.stringify(requestBody)
+  });
+
+  // console.log('Response Body:', await response.text());
+
+  // Check if the response is successful
+  if (!response.ok) {
+    throw new Error(`Failed to fetch session data: ${response.statusText}`);
+  }
+
+  // Parse the JSON response
+  const responseData = await response.json();
+
+  // Check if the response contains the TokenId
+  if (responseData && responseData.IsSuccess && responseData.Data && responseData.Data.TokenId) {
+    // Store the TokenId into the cache
+
+    await cache.put(tokenRequest, new Response(responseData.Data.TokenId));
+    // Return the session data from the API response
+    console.log(" token generated");
+    return responseData.Data.TokenId;
+  } else {
+    throw new Error('Session data retrieval failed: TokenId not found in the response');
+  }
+}
+
+async function removeFromCache() {
+  const cache = caches.default;
+  await cache.delete(tokenRequest);
+}
+
+async function getCachedAuthHeader(url, env) {
+  // Retrieve auth header from cache
+  const cache = caches.default;
+  const cachedResponse = await cache.match(TOKEN_ID);
+  if (cachedResponse) {
+    const cachedData = await cachedResponse.text();
+    return cachedData;
+  } else {
+    const cachedData =  await getSessionData(url, cache, env);
+    return cachedData;
+  }
+}
 const handleRequest = async (request, env) => {
-  if (request.method === 'OPTIONS') {
+
+
+
+  if (request.method === "OPTIONS") {
     // Handle CORS preflight requests
-    // eslint-disable-next-line no-use-before-define
     return handleOptions(request);
   }
   if (request.method === 'POST') {
     try {
       const body = await request.json(); // Extract JSON body from the incoming request
-      const isMarketAPI = request.url.includes('CDNMarketAPI'); // Check if the URL contains "CDNMarketAPI"
+      const isMarketAPI = request.url.includes("CDNMarketAPI"); // Check if the URL contains "CDNMarketAPI"
       let authHeader;
       let newUrl = env.ORIGIN_HOSTNAME + request.url.substr(request.url.indexOf('/', 8));
       if (isMarketAPI) {
         authHeader = env.Authorization_Marketing;
       } else {
-        authHeader = env.Authorization_Research;
+        authHeader = await getCachedAuthHeader(newUrl, env);
       }
 
-      if (newUrl === `${env.ORIGIN_HOSTNAME}/`) {
+      if (newUrl === env.ORIGIN_HOSTNAME + "/") {
         // If newUrl is just env.ORIGIN_HOSTNAME + "/", add "CDNResearchAPI/CallResearchAPI" to it
-        newUrl += 'CDNResearchAPI/CallResearchAPI';
+        newUrl += "CDNResearchAPI/CallResearchAPI";
       }
 
       // Set up the new request to forward the data
       const init = {
         method: 'POST',
         headers: {
-          Authorization: authHeader, // Use the Authorization token from the environment variable
+          'Authorization': authHeader, // Use the Authorization token from the environment variable
           'Content-Type': 'application/json', // Set Content-Type to application/json
         },
         body: JSON.stringify(body), // Forward the original request body as JSON
@@ -45,15 +111,31 @@ const handleRequest = async (request, env) => {
 
       // Forward the request to the new URL
       const response = await fetch(newUrl, init);
+      //console.log('Response Body:', await response.text());
 
+      const responseBody = await response.json();
+      console.log("I am here2");
+      if (responseBody.Message === "Session expired. Please create new session.") {
+        // Session expired, remove token from cache
+        console.log("I am here3");
+        await removeFromCache();
+        // Call handleOptions to trigger token generation
+        return handleOptions(request, env);
+      }
+
+      console.log("I am here4 " + JSON.stringify(responseBody));
       // Return the response from the forwarded request back to the original client
-      return new Response(response.body, {
+      console.log(response.status);
+      console.log(response.headers);
+      const newResponse = new Response(JSON.stringify(responseBody), {
         status: response.status,
-        headers: response.headers,
+        headers: response.headers
       });
+      return newResponse;
+
     } catch (error) {
       // Handle any errors that might occur during the request forwarding
-      return new Response(`Error forwarding request: ${error.message}`, { status: 500 });
+      return new Response('Error forwarding request: ' + error.message, { status: 500 });
     }
   } else if (request.method === 'GET') {
     try {
@@ -67,26 +149,29 @@ const handleRequest = async (request, env) => {
         status: response.status,
         headers: response.headers,
       });
+
     } catch (error) {
       // Handle any errors that might occur during the request forwarding
-      return new Response(`Error fetching data: ${error.message}`, { status: 500 });
+      return new Response('Error fetching data: ' + error.message, { status: 500 });
     }
-  } else {
+
+  }  else {
     // If not a POST or GET request, return 405 Method Not Allowed
     return new Response('Method Not Allowed', { status: 405 });
   }
-};
+}
 
-// eslint-disable-next-line no-unused-vars
 async function handleOptions(request) {
   // Handle CORS preflight requests.
   return new Response(null, {
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*',
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Headers" : "*",
     },
   });
+
 }
+
 
 export default {
   fetch: handleRequest,
