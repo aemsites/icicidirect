5480e6111789c0afa6a3e8be4450ca6c274770cd
anagarwa <anagarwa@adobe.com>
2024-05-10 11:24:23 +0530

    fix: checking selector

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -189,10 +189,11 @@ async function getAndApplyRenderDecisions() {
   // so we can hook up into the AEM EDS page load sequence
   const response = await window.alloy('sendEvent', { renderDecisions: false });
   const { propositions } = response;
-  propositions.forEach((p) => {
+  const clonedPropositions = Object.assign([], propositions);
+  clonedPropositions.forEach((p) => {
     p.items = p.items.filter((i) => i.schema !== 'https://ns.adobe.com/personalization/dom-action' || !getElementForProposition(i));
   });
-  
+
   onDecoratedElement(async () => {
     await window.alloy('applyPropositions', { propositions });
     // keep track of propositions that were applied
