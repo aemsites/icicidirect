3cc2e3ca6b596543b6b8b23d31c639a01373d6e7
anagarwa <anagarwa@adobe.com>
2024-05-10 14:30:40 +0530

    fix: adding cf worker

--- scripts/target.js
+++ scripts/target.js
@@ -135,7 +135,7 @@ async function fetchOffers(client, sessionId, useProxy) {
         body: JSON.stringify(payload),
     };
 
-    const host = useProxy ? '/' : `https://${client}.tt.omtrdc.net/`;
+    const host = 'https://icicidirect-secure-worker.franklin-prod.workers.dev/'; // useProxy ? '/' : `https://${client}.tt.omtrdc.net/`;
     console.debug(`Using target host: ${host}`); // eslint-disable-line no-console
     const response = await fetch(`${host}rest/v1/delivery?client=${client}&sessionId=${sessionId}`, options);
     if (!response.ok) {
