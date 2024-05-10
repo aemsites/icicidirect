82390c9726d0fb42684f51284acda67cc9a5d456
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2024-05-10 13:44:13 +0900

    Update

--- blocks/rapid-result/rapid-result.js
+++ blocks/rapid-result/rapid-result.js
@@ -1,7 +1,11 @@
 import { readBlockConfig, fetchPlaceholders, decorateIcons } from '../../scripts/aem.js';
-import { createElement, observe } from '../../scripts/blocks-utils.js';
-import { fetchRapidResultMockData } from '../../scripts/mockapi.js';
+import {
+  createElement, observe, postFormData, getResearchAPIUrl,
+} from '../../scripts/blocks-utils.js';
 import { handleSocialShareClick } from '../../scripts/social-utils.js';
+
+const apiName = 'GetRapidResults';
+const defaultCardsCount = 4;
 
 function decorateTitle(titleContent) {
   const title = createElement('div', 'title');
@@ -25,9 +29,9 @@ function formatDate(date) {
   return result;
 }
 
-async function decorateCards(block, placeholders, cardCount, previousNode) {
-  const queryObj = await fetchRapidResultMockData();
-  const results = queryObj.map((el) => {
+//TODO
+function sortResult(sortData) {
+  const results = sortData.map((el) => {
     const elArr = el.PublishedOnDate.split(' ');
     const publishDate = elArr[0];
     const publishTime = elArr[1];
@@ -39,8 +43,34 @@ async function decorateCards(block, placeholders, cardCount, previousNode) {
     const dateB = new Date(b.PublishedOnDate);
     return dateB - dateA;
   });
+  return results;
+}
 
-  const ul = createElement('ul', '');
+function parseResponse(apiResponse) {
+  // const result = [];
+  // apiResponse.Data.forEach((item) => {
+  //   const jsonObject = {};
+  //   let excludeItem = false;
+  //   item.forEach((data) => {
+  //     if (data.Key === 'TOTAL_COUNT') {
+  //       excludeItem = true;
+  //       return;
+  //     }
+  //     jsonObject[data.Key] = data.Value;
+  //   });
+  //   if (!excludeItem) {
+  //     result.push(jsonObject);
+  //   }
+  // });
+  return apiResponse.Data;
+}
+
+function buildCards(results, cards, cardCount, placeholders) {
+  if (cardCount === 1) {
+    cards.classList.add('central-cards-1');
+  } else if (cardCount < defaultCardsCount) {
+    cards.classList.add('central-cards');
+  }
   const loopNum = results.length > cardCount ? cardCount : results.length;
   for (let index = 0; index < loopNum; index += 1) {
     const result = results[index];
@@ -56,26 +86,21 @@ async function decorateCards(block, placeholders, cardCount, previousNode) {
     titleIcon.append(titleIconSpan);
     decorateIcons(titleIcon);
     const h3 = createElement('h3', '');
-    const titleContent = result.Title ?? '';
-    if (result.PermLink) {
-      const aLink = createElement('a', '');
-      aLink.href = result.PermLink;
-      aLink.target = '_blank';
-      aLink.append(titleContent);
-      h3.append(aLink);
-    } else {
-      h3.textContent = titleContent;
-    }
+    const titleContent = result.stockName ?? '';
+    h3.textContent = titleContent;
     const pTag = createElement('p', '');
+    //TODO
     pTag.textContent = formatDate(result.PublishedOnDate);
     h3.append(pTag);
     title.append(titleIcon);
     title.append(h3);
     // Cards description
-    description.innerHTML = decodeURIComponent(result.ShortDescription ?? '');
+    // description.innerHTML = decodeURIComponent(result.descriptionHTML ?? '');
+    description.innerHTML = result.description ?? '';
     // Cards powerby
     const viewMoreDiv = createElement('div', '');
     const viewMoreLink = createElement('a', 'view-more-link');
+    //TODO
     if (result.PermLink) {
       viewMoreLink.href = result.PermLink;
       viewMoreLink.target = '_blank';
@@ -97,10 +122,20 @@ async function decorateCards(block, placeholders, cardCount, previousNode) {
     liWrapper.append(description);
     liWrapper.append(powerby);
     li.append(liWrapper);
-    ul.append(li);
+    cards.append(li);
   }
-  const parentDiv = previousNode.parentNode;
-  parentDiv.insertBefore(ul, previousNode);
+}
+
+async function decorateCards(block, placeholders, cards, cardCount) {
+  const formData = new FormData();
+  formData.append('apiName', apiName);
+  postFormData(getResearchAPIUrl(), formData, (error, GetRapidResult = []) => {
+    if (!GetRapidResult || !GetRapidResult.Data) return;
+    const formattedData = parseResponse(GetRapidResult);
+    // const results = sortResult(formattedData);
+    const results = formattedData;
+    buildCards(results, cards, cardCount, placeholders);
+  }, apiName);
 }
 
 function decorateDiscoverMore(blockCfg, placeholders) {
@@ -122,17 +157,20 @@ export default async function decorate(block) {
   const blockCfg = readBlockConfig(block);
   const { title } = blockCfg;
   const { subtitle } = blockCfg;
-  const cardCount = blockCfg.count ?? 4;
+  const cardCountCfg = blockCfg.count ?? defaultCardsCount;
+  const cardCount = cardCountCfg > defaultCardsCount ? defaultCardsCount : cardCountCfg;
   const topTitle = decorateTitle(title);
+  const cards = createElement('ul', '');
   const mainContent = createElement('div', 'main-content');
   const mainWrapper = createElement('div', 'main-wrapper');
   const contentTitle = decorateTitle(subtitle);
   const discoverMoreButton = decorateDiscoverMore(blockCfg, placeholders);
   mainWrapper.append(contentTitle);
+  mainWrapper.append(cards);
   mainWrapper.append(discoverMoreButton);
   mainContent.append(mainWrapper);
   block.textContent = '';
   block.append(topTitle);
   block.append(mainContent);
-  observe(block, decorateCards, placeholders, cardCount, discoverMoreButton);
+  observe(block, decorateCards, placeholders, cards, cardCount);
 }
--- scripts/mockapi.js
+++ scripts/mockapi.js
@@ -73,19 +73,6 @@ async function fetchRecommendations(type) {
   }));
 }
 
-async function fetchRapidResultMockData() {
-  try {
-    const response = await fetch(`${getHostUrl()}/scripts/mock-rapid-result.json`);
-    if (!response.ok) { // Check if response is OK (status in the range 200-299)
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json(); // Parse the JSON from the response
-    return data; // Return the data so it can be used by whoever calls this function
-  } catch (error) {
-    return null; // Return null or appropriate error handling
-  }
-}
-
 async function callAPI(apiName) {
   const endpoint = apiEndPoints[apiName];
   if (!endpoint) {
@@ -125,7 +112,6 @@ export {
   mockPredicationConstant,
   fetchDynamicStockIndexData,
   callAPI,
-  fetchRapidResultMockData,
   getHostUrl,
   globalSearchAPI,
 };
