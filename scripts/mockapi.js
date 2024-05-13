1da149de697712caaead54e0fd274c2dff7c4623
anagarwa <anagarwa@adobe.com>
2024-05-13 13:37:03 +0530

    fix: removing unused code from mockapi

--- scripts/mockapi.js
+++ scripts/mockapi.js
@@ -1,78 +1,4 @@
-const marginActions = {
-  sell: 'https://secure.icicidirect.com/trading/equity/marginsell',
-  buy: 'https://secure.icicidirect.com/trading/equity/marginbuy',
-};
-
-const mockPredicationConstant = {
-  recoPrice: 'Reco. Price',
-  cmp: 'CMP',
-  targetPrice: 'Target Price',
-  stopLoss: 'Stop Loss',
-  profitPotential: 'Profit Potential',
-  returns: 'Returns',
-  action: 'Action',
-  profitExit: 'Call Closed and Book Profit Price:',
-  bookProfit: 'Call closed and Book Profit price:',
-  minAmount: 'Min. Amount',
-  riskProfile: 'Risk Profile',
-  buyingRange: 'Buying Range',
-
-};
-
-const apiEndPoints = {
-  trading: '/draft/anagarwa/tradingtips.json',
-  investing: '/draft/anagarwa/investingideas.json',
-  oneclickportfolio: '/draft/anagarwa/oneclickportfolio.json',
-  muhratpicks: '/draft/anagarwa/muhratpicks.json',
-  finace: '/draft/shroti/finace.json',
-  rapidresult: '/draft/jiang/rapidresult.json',
-};
-
-function getHostUrl() {
-  let hostUrl = window.location.origin;
-  if (!hostUrl || hostUrl === 'null') {
-    // eslint-disable-next-line prefer-destructuring
-    hostUrl = window.location.ancestorOrigins[0];
-  }
-  return hostUrl;
-}
-
-async function fetchDataFromAPI(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return await response.json();
-  } catch (error) {
-    return null;
-  }
-}
-
-async function fetchRecommendations(type) {
-  const hostUrl = getHostUrl();
-  const apiUrl = `${hostUrl}${apiEndPoints[type]}`;
-  const data = await fetchDataFromAPI(apiUrl);
-  if (!data) {
-    return [];
-  }
-  // Transform the API response to the desired companies array format
-  return data.data.map((company) => ({
-    action: company.action,
-    name: company.company,
-    recoPrice: company.recoPrice,
-    cmp: company.cmp,
-    targetPrice: company.targetPrice,
-    stopLoss: company.stopLoss,
-    exit: company.exit,
-    reportLink: company.reportLink,
-    profitPotential: company.profitPotential,
-    returns: company.returns,
-    minAmount: company.minAmount,
-    riskProfile: company.riskProfile,
-    buyingRange: company.buyingRange,
-  }));
-}
+import { getHostUrl } from './blocks-utils.js';
 
 async function fetchRapidResultMockData() {
   try {
@@ -85,22 +11,6 @@ async function fetchRapidResultMockData() {
   } catch (error) {
     return null; // Return null or appropriate error handling
   }
-}
-
-async function callAPI(apiName) {
-  const endpoint = apiEndPoints[apiName];
-  if (!endpoint) {
-    return null;
-  }
-  return fetchDataFromAPI(endpoint);
-}
-
-function getMarginActionUrl(actionName) {
-  return marginActions[actionName];
-}
-
-async function globalSearchAPI() {
-  return fetchDataFromAPI(`${getHostUrl()}/scripts/mock-global-search.json`);
 }
 
 const fetchDynamicStockIndexData = () => [
@@ -121,12 +31,6 @@ const fetchDynamicStockIndexData = () => [
 ];
 
 export {
-  fetchRecommendations,
-  getMarginActionUrl,
-  mockPredicationConstant,
   fetchDynamicStockIndexData,
-  callAPI,
   fetchRapidResultMockData,
-  getHostUrl,
-  globalSearchAPI,
 };
