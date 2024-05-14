1ccc9a2a0639eb169493f9903217c1809c82808d
anagarwa <anagarwa@adobe.com>
2024-05-14 14:35:27 +0530

    fix: allowing limited origin of aem

--- tools/api-proxy/secureapi-proxy.js
+++ tools/api-proxy/secureapi-proxy.js
@@ -8,19 +8,17 @@
  * @param env
  * @returns {Promise<Response>}
  */
-/* eslint-disable */
-// eslint-disable-next-line no-unused-vars
-    // e
+
 const TOKEN_ID = 'http://icicidirect.finoux.com/CDNResearchAPI/CallResearchAPI.TokenId';
 
 const tokenRequest = new Request(TOKEN_ID, {
-  method: 'GET'
+  method: 'GET',
 });
 
 async function getSessionData(url, cache, env) {
   // Define the request body
   const requestBody = {
-    apiName: "GenerateSession"
+    apiName: 'GenerateSession',
   };
 
   // Make a POST request to the API to fetch session data
@@ -29,11 +27,10 @@ async function getSessionData(url, cache, env) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': env.Authorization_Research,
+      Authorization: env.Authorization_Research,
     },
-    body: JSON.stringify(requestBody)
+    body: JSON.stringify(requestBody),
   });
-
 
   // Check if the response is successful
   if (!response.ok) {
@@ -50,9 +47,8 @@ async function getSessionData(url, cache, env) {
     await cache.put(tokenRequest, new Response(responseData.Data.TokenId));
     // Return the session data from the API response
     return responseData.Data.TokenId;
-  } else {
-    throw new Error('Session data retrieval failed: TokenId not found in the response');
   }
+  throw new Error('Session data retrieval failed: TokenId not found in the response');
 }
 
 async function removeFromCache() {
@@ -67,13 +63,23 @@ async function getCachedAuthHeader(url, env) {
   if (cachedResponse) {
     const cachedData = await cachedResponse.text();
     return cachedData;
-  } else {
-    const cachedData =  await getSessionData(url, cache, env);
-    return cachedData;
   }
+  const cachedData = await getSessionData(url, cache, env);
+  return cachedData;
 }
-const handleRequest = async (request, env) => {
 
+// eslint-disable-next-line no-unused-vars
+async function handleOptions(request) {
+  // Handle CORS preflight requests.
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+    },
+  });
+}
+
+const handleRequest = async (request, env) => {
   const allowedOriginRegex = /^(https:\/\/.*\.(hlx\.live|hlx\.page|aem\.live|aem\.page))$/;
 
   const requestOrigin = request.headers.get('Origin');
@@ -84,16 +90,14 @@ const handleRequest = async (request, env) => {
     return new Response('Forbidden', { status: 403 });
   }
 
-
-
-  if (request.method === "OPTIONS") {
+  if (request.method === 'OPTIONS') {
     // Handle CORS preflight requests
     return handleOptions(request);
   }
   if (request.method === 'POST') {
     try {
       const body = await request.json(); // Extract JSON body from the incoming request
-      const isMarketAPI = request.url.includes("CDNMarketAPI"); // Check if the URL contains "CDNMarketAPI"
+      const isMarketAPI = request.url.includes('CDNMarketAPI'); // Check if the URL contains "CDNMarketAPI"
       let authHeader;
       let newUrl = env.ORIGIN_HOSTNAME + request.url.substr(request.url.indexOf('/', 8));
       if (isMarketAPI) {
@@ -102,16 +106,16 @@ const handleRequest = async (request, env) => {
         authHeader = await getCachedAuthHeader(newUrl, env);
       }
 
-      if (newUrl === env.ORIGIN_HOSTNAME + "/") {
+      if (newUrl === `${env.ORIGIN_HOSTNAME}/`) {
         // If newUrl is just env.ORIGIN_HOSTNAME + "/", add "CDNResearchAPI/CallResearchAPI" to it
-        newUrl += "CDNResearchAPI/CallResearchAPI";
+        newUrl += 'CDNResearchAPI/CallResearchAPI';
       }
 
       // Set up the new request to forward the data
       const init = {
         method: 'POST',
         headers: {
-          'Authorization': authHeader, // Use the Authorization token from the environment variable
+          Authorization: authHeader, // Use the Authorization token from the environment variable
           'Content-Type': 'application/json', // Set Content-Type to application/json
         },
         body: JSON.stringify(body), // Forward the original request body as JSON
@@ -121,7 +125,7 @@ const handleRequest = async (request, env) => {
       const response = await fetch(newUrl, init);
 
       const responseBody = await response.json();
-      if (responseBody.Message === "Session expired. Please create new session.") {
+      if (responseBody.Message === 'Session expired. Please create new session.') {
         // Session expired, remove token from cache
         await removeFromCache();
         // Call handleOptions to trigger token generation
@@ -131,13 +135,12 @@ const handleRequest = async (request, env) => {
       // Return the response from the forwarded request back to the original client
       const newResponse = new Response(JSON.stringify(responseBody), {
         status: response.status,
-        headers: response.headers
+        headers: response.headers,
       });
       return newResponse;
-
     } catch (error) {
       // Handle any errors that might occur during the request forwarding
-      return new Response('Error forwarding request: ' + error.message, { status: 500 });
+      return new Response(`Error forwarding request: ${error.message}`, { status: 500 });
     }
   } else if (request.method === 'GET') {
     try {
@@ -151,29 +154,15 @@ const handleRequest = async (request, env) => {
         status: response.status,
         headers: response.headers,
       });
-
     } catch (error) {
       // Handle any errors that might occur during the request forwarding
-      return new Response('Error fetching data: ' + error.message, { status: 500 });
+      return new Response(`Error fetching data: ${error.message}`, { status: 500 });
     }
-
-  }  else {
+  } else {
     // If not a POST or GET request, return 405 Method Not Allowed
     return new Response('Method Not Allowed', { status: 405 });
   }
-}
-
-async function handleOptions(request) {
-  // Handle CORS preflight requests.
-  return new Response(null, {
-    headers: {
-      "Access-Control-Allow-Origin" : "*",
-      "Access-Control-Allow-Headers" : "*",
-    },
-  });
-
-}
-
+};
 
 export default {
   fetch: handleRequest,
