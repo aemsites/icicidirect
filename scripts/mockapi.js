c4e73cfc0dfb07f13c716f28e5acd1e02b6bbea9
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2024-05-14 18:56:41 +0900

    Roll back mock api changes

--- scripts/mockapi.js
+++ scripts/mockapi.js
@@ -74,6 +74,19 @@ async function fetchRecommendations(type) {
   }));
 }
 
+async function fetchRapidResultMockData() {
+  try {
+    const response = await fetch(`${getHostUrl()}/scripts/mock-rapid-result.json`);
+    if (!response.ok) { // Check if response is OK (status in the range 200-299)
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json(); // Parse the JSON from the response
+    return data; // Return the data so it can be used by whoever calls this function
+  } catch (error) {
+    return null; // Return null or appropriate error handling
+  }
+}
+
 async function callAPI(apiName) {
   const endpoint = apiEndPoints[apiName];
   if (!endpoint) {
@@ -113,6 +126,7 @@ export {
   mockPredicationConstant,
   fetchDynamicStockIndexData,
   callAPI,
+  fetchRapidResultMockData,
   getHostUrl,
   globalSearchAPI,
 };
