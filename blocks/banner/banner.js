140c64b5697ff1d5b4c26d59ced5fc1d60f68d77
Vivek Singh <vivesing@adobe.com>
2024-05-20 16:29:53 +0530

    Address review comments

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,10 +1,8 @@
 export default async function decorate(block) {
-  block.display = 'none';
   const bannerLink = block.querySelector('a');
   bannerLink.innerText = '';
   const bannerImage = block.querySelector('picture');
   bannerLink.append(bannerImage);
   block.innerText = '';
   block.append(bannerLink);
-  block.display = 'block';
 }
