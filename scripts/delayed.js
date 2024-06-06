dff612ad6370928173300038a41dfb7caf0e9af0
anagarwa <anagarwa@adobe.com>
2024-06-06 17:47:36 +0530

    adding recaptcha to further delay

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -10,41 +10,29 @@ import {
 sampleRUM('cwv');
 
 const loadMartech = getQueryParam(LOAD_MARTECH_PARAM) ?? 'all';
-if (loadMartech === 'off') {
-  return;
-}
-
-const isSidekickLibrary = (window.location.pathname.includes('srcdoc'));
-
-const onCaptchaloadCallback = () => {
-  document.querySelectorAll('.g-recaptcha').forEach((el) => {
-    fetchPlaceholders().then((placeholders) => {
-      // eslint-disable-next-line no-undef
-      grecaptcha.render(el, {
-        sitekey: placeholders.sitekey, // Use sitekey from placeholders
-        callback(token) {
-          window.validateCaptchaToken = token;
-        },
+if (loadMartech !== 'off') {
+  const isSidekickLibrary = (window.location.pathname.includes('srcdoc'));
+  const onCaptchaloadCallback = () => {
+    document.querySelectorAll('.g-recaptcha').forEach((el) => {
+      fetchPlaceholders().then((placeholders) => {
+        // eslint-disable-next-line no-undef
+        grecaptcha.render(el, {
+          sitekey: placeholders.sitekey, // Use sitekey from placeholders
+          callback(token) {
+            window.validateCaptchaToken = token;
+          },
+        });
       });
     });
-  });
-};
-
-window.onCaptchaloadCallback = onCaptchaloadCallback;
-
-
-
-/**
- * Google Tag Manager
-* */
-
-loadScript('/scripts/cookie-script.js');
-
-if (!isSidekickLibrary) {
-  if (!defaultAnalyticsLoadDisabled()) {
-    loadAdobeLaunchAndGTM();
+  };
+  window.onCaptchaloadCallback = onCaptchaloadCallback;
+  loadScript('/scripts/cookie-script.js');
+  if (!isSidekickLibrary) {
+    if (!defaultAnalyticsLoadDisabled()) {
+      loadAdobeLaunchAndGTM();
+    }
+    // Needed for chatbot to work in non-prod environments
+    loadScript('/scripts/mockxmlhttprequest.js', {type: 'module'});
+    loadScript('https://icici-securities.allincall.in/files/deploy/embed_chatbot_11.js?version=1.1');
   }
-  // Needed for chatbot to work in non-prod environments
-  loadScript('/scripts/mockxmlhttprequest.js', { type: 'module' });
-  loadScript('https://icici-securities.allincall.in/files/deploy/embed_chatbot_11.js?version=1.1');
 }
